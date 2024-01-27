754fb22c3fcfd028b8a70d8854f3eb5b50c6a430
eren <115615808+ErenAtasun@users.noreply.github.com>
2024-01-27 17:50:15 +0300

    PlayerHealth

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public float health;
+    public float maxHealth;
+    void Start()
+    {
+        maxHealth = health;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
